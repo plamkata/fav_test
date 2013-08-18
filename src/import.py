6c1b76159dc88b3eba8c3f5df7095008fa2a0803
Plamen Aleksandrov <paleksandrov@Plamens-MacBook-Air.local>
2013-08-18 12:52:05 +0300

    Initial import functions and cql driver for python

---
+++ src/import.py
@@ -0,0 +1,36 @@
+'''
+Import sample csv data into Cassandra.
+
+Created on Aug 18, 2013
+@author: paleksandrov
+'''
+import cql
+
+def main():
+    path = '/Users/paleksandrov/Documents/SoundCloud/Cassandra/Data/favoritings.csv'
+        
+    con = None
+    cursor = None
+    try:
+        con = cql.connect('localhost', 9160, 'fav_test', cql_version='3.0.0')
+        print ("Connected!")
+        cursor = con.cursor()
+        CQLString = "INSERT INTO event_attend (event_id, event_type, event_user_id) VALUES (131, 'Party', 3156);"
+        cursor.execute(CQLString)
+    finally:
+        if cursor : cursor.close()
+        if con : con.close()
+    
+    with open(path, 'rb') as csvfile:
+        rownum = 0
+        
+        # Skip header
+        next(csvfile)
+        for line in csvfile:
+            print str(rownum) + ',' + line
+            
+            rownum += 1
+            if (rownum >= 100) : break
+
+if __name__ == '__main__':
+    main()
